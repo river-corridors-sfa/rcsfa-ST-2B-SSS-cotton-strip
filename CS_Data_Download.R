5b5efe50fdd86e9ab851ef992c6a90ecc004c7d8
stegen <James.Stegen@pnnl.gov>
2024-12-20 08:35:28 -0800

    Create CS_Data_Download.R

---
+++ CS_Data_Download.R
@@ -0,0 +1,8 @@
+# Load devtools package
+library(devtools)
+
+# Specify the GitHub URL of the script
+github_url <- "https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-essdive-api/main/ESS-DIVE_Download_R/script_ess_dive_file_download_function.R"
+
+# Source the script from GitHub
+source_url(github_url)
