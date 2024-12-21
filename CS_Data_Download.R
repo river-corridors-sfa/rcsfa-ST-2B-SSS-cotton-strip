e9d168ea719c5ec48cbba965685f5b68c3fc9e96
stegen <James.Stegen@pnnl.gov>
2024-12-20 18:52:23 -0800

    Data package (v3) and some code

--- CS_Data_Download.R
+++ CS_Data_Download.R
@@ -2,7 +2,12 @@
 library(devtools)
 
 # Specify the GitHub URL of the script
-github_url <- "https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-essdive-api/main/ESS-DIVE_Download_R/script_ess_dive_file_download_function.R"
+github_url <- "https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-data_processing_for_publication/refs/heads/main/Data_Package_ESS-DIVE/download_from_ESS-DIVE_landing_page/ESS-DIVE_Download_R/script_ess_dive_file_download_function.R"
 
 # Source the script from GitHub
 source_url(github_url)
+
+csv_files_from_data_package <- download_and_read_data(
+  target_url = "https://data.ess-dive.lbl.gov/catalog/d1/mn/v2/object/ess-dive-f8b4bf15fc21108-20241107T161454014", # the url from ess-dive
+  filename = "v3_SSS_Data_Package.zip", # your choice for how the downloaded zip will be named
+  downloads_folder = getwd())
---
+++ SSS_Cotton_Map_Rivers_ML_BF_JCS.R
@@ -0,0 +1,203 @@
+# ==============================================================================
+#
+# Make map for Cotton Strips
+#
+# Status: Complete
+#
+# ==============================================================================
+#
+# Author: Brieanne Forbes 
+# 4 August 2023
+#
+# ==============================================================================
+library(tidyverse) #keep it tidy
+library(raster) # work with rasters, NOTE: masks dplyr::select
+library(janitor) # clean_names()
+library(ggthemes) # theme_map()
+library(ggsflabel) # add labels to sf objects
+library(ggnewscale) # set multiple color scales
+library(ggspatial) # add north arrow and scale bar
+library(nhdplusTools) # get watershed boundary/flowlines
+library(elevatr) # pull elevation maps
+library(sf) # tidy spatial
+library(spData)
+library(cowplot)
+library(rstudioapi)
+library(viridis)
+
+
+rm(list=ls(all=T))
+
+# Setting wd to parent folder
+current_path <- rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path))
+setwd("./..")
+
+# ================================= User inputs ================================
+
+metadata_file <- 'C:/GitHub/CottonStripPaper/data/v2_SSS_Data_Package/v2_SSS_Field_Metadata.csv'
+
+data_file <- 'C:/GitHub/CottonStripPaper/data/Exported Data/Decay_Data.csv'
+
+summary = read.csv("C:/GitHub/CottonStripPaper/data/v2_SSS_Data_Package/SSS_CottonStrip_TensileStrength_DecayRate_Summary.csv", skip = 2, header = TRUE)
+
+summary = summary %>% 
+  mutate(Sample_Name = stringr::str_extract(Sample_Name,"SSS[0-9]{3}")) %>% 
+  filter(!row_number() %in% c(1:11)) %>% 
+  dplyr::select(Sample_Name, Mean_Decay_Rate_per_day)
+
+
+yrb_shp_dir <- 'C:/GitHub/CottonStripPaper/data/YakimaRiverBasin_Boundary/'
+
+cluster_shp_dir <- 'C:/GitHub/CottonStripPaper/data/YRB_Cluster/'
+
+common_crs = 4326
+
+# ============================== read in and merge =============================
+
+metadata <- read_csv(metadata_file) %>%
+  dplyr::select(Parent_ID, Site_ID, Latitude, Longitude)
+
+data <- read_csv(data_file) %>%
+  dplyr::select(Parent_ID, mean_degree_decay_rate) %>% 
+  distinct(Parent_ID, .keep_all = TRUE)
+
+merge <- data %>%
+  left_join(metadata, by = c('Parent_ID' = 'Parent_ID')) 
+
+# %>%
+#   mutate(ER_wc = if_else(ER_wc > 0, 0, ER_wc))
+  
+# ============================ read in YRB shp file ============================
+
+YRB_shp <- list.files(yrb_shp_dir, 'Basin.shp', full.names = T)
+
+YRB_boundary <- read_sf(YRB_shp) %>%
+  st_transform(common_crs)
+
+# ============================ convert to sf object ============================
+
+sites <- st_as_sf(merge, coords = c('Longitude','Latitude'), crs = common_crs)
+
+# ======================== pull NHD data and elevation =========================
+
+geometry <- 
+
+YRB_flowlines <- get_nhdplus(AOI = YRB_boundary$geometry, streamorder = 3)
+
+elevation_raw <- get_elev_raster(YRB_boundary$geometry, z = 10)
+
+elevation_crop <- mask(elevation_raw, YRB_boundary)
+
+elevation <- as.data.frame(elevation_crop, xy = T) %>%
+  as_tibble() %>%
+  rename("long" = x,
+         "lat" = y,
+         "elevation" = 3) %>% #column index > name (changing resolution changes colname)
+  filter(!is.na(elevation))
+
+# ============================ read in cluster shp file ========================
+
+cluster_shp <- list.files(cluster_shp_dir, 'shp', full.names = T)
+
+cluster <- read_sf(cluster_shp) %>%
+  st_transform(common_crs)
+
+
+# ========================= create insert map ======================
+
+data("us_states", package = "spData")
+us_states_4326 = st_transform(us_states, crs = 4326)
+
+wa <- us_states_4326 %>% filter(NAME == "Washington")
+
+insert <- ggplot() +
+  geom_sf(data = us_states_4326, fill = "white") + 
+  geom_sf(data = wa, fill = "black",colour = "black")+
+  geom_sf(data = YRB_boundary, colour = "red", fill = 'red') +
+  labs(x = "", y = "")+
+  theme_map()
+
+# ========================= create map of ER water column (elevation) ======================
+
+ER_wc_map <- ggplot()+
+  geom_sf(data = YRB_boundary, fill = "white")+
+  #geom_raster(data = elevation, aes(long, lat, fill = elevation), show.legend = F, alpha = 0.4)+
+  #scale_fill_gradient(low = 'white', high = 'black')+
+  geom_sf(data = YRB_flowlines, color = "royalblue", alpha = 0.6)+
+  new_scale_fill()+
+  geom_sf(data = sites, aes(color = mean_degree_decay_rate, size = mean_degree_decay_rate), show.legend = T) +
+  scale_fill_viridis(option = 'B', begin = 0.2, direction = -1)+
+  scale_color_viridis(option = 'B', begin = 0.2, direction = -1)+
+  scale_size(range = c(1.5, 6))+
+  theme_map() +
+  labs(x = "", y = "", color = bquote(K[dd])) +
+  guides(size = "none")+
+  ggspatial::annotation_scale(
+    location = "br",
+    pad_x = unit(0.5, "in"),
+    bar_cols = c("black", "white")) +
+  ggspatial::annotation_north_arrow(
+    location = "tl", which_north = "true",
+    pad_x = unit(1.5, "in"),
+    # pad_y = unit(0.5, "in"),
+    style = ggspatial::north_arrow_nautical(
+      fill = c("black", "white"),
+      line_col = "grey20"))
+
+full <- ggdraw() +
+  draw_plot(ER_wc_map) +
+  draw_plot(insert, x = 0.4, y = 0.4, width = 0.3, height = 0.3)
+
+ggsave('./Data/Maps/SPS_ER_Water_Column_Map.pdf',
+       full,
+       width = 8,
+       height = 5
+)
+
+# ========================= create map of ER water column (cluster) ======================
+
+ER_wc_map_cluster <- ggplot()+
+  geom_sf(data = YRB_boundary)+
+  geom_sf(data = cluster, aes(fill = as.factor(ClusterNum), color = as.factor(ClusterNum)), show.legend = T)+
+  # scale_fill_manual(values = alpha(c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'), 0.3))+
+  # scale_color_manual(values = alpha(c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'), 0.2))+
+  scale_fill_manual(values = c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'))+
+  scale_color_manual(values = c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'))+
+  geom_sf(data = YRB_flowlines, color = "royalblue")+
+  new_scale_fill()+
+  new_scale_color()+
+  # geom_sf(data = sites, aes(color = ER_wc, size = ER_wc), show.legend = T) +
+  # geom_sf(data = sites, aes(size = ER_wc), show.legend = T, shape = 18, fill = 'white', color = 'black') +
+  geom_sf(data = sites, show.legend = T, size = 3) +
+  # scale_fill_viridis(option = 'B', begin = 0.3)+
+  # scale_color_viridis(option = 'B', begin = 0.3)+
+  # scale_fill_gradient(low = 'white', high = 'black')+
+  # scale_color_gradient(low = 'white', high = 'black')+
+  # scale_size(range = c(0.1, 10), trans = 'reverse')+
+  theme_map() +
+  # labs(x = "", y = "", color = "Water Column\nRespiration\n(mg O2 L-1 day-1)") +
+  ggspatial::annotation_scale(
+    location = "br",
+    pad_x = unit(0.5, "in"),
+    bar_cols = c("black", "white")) +
+  ggspatial::annotation_north_arrow(
+    location = "tl", which_north = "true",
+    pad_x = unit(1.5, "in"),
+    # pad_y = unit(0.5, "in"),
+    style = ggspatial::north_arrow_nautical(
+      fill = c("black", "white"),
+      line_col = "grey20"))
+
+full_cluster <- ggdraw() +
+  draw_plot(ER_wc_map_cluster) +
+  draw_plot(insert, x = 0.4, y = 0.4, width = 0.3, height = 0.3)
+
+ggsave('./Data/Maps/SPS_ER_Water_Column_Map_Cluster.pdf',
+       full_cluster,
+       width = 8,
+       height = 5
+)
+
+
+
